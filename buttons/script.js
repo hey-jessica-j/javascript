62106ac9ac4ecc316ffa383af41a4178a6527267
hey-jessica-j <hellojessicajennings@gmail.com>
2021-02-07 20:59:06 -0500

    first commit

---
+++ buttons/script.js
@@ -0,0 +1,17 @@
+
+
+function changeColorBlue() {
+  document.body.style.backgroundColor = "blue";
+}
+
+function changeColorRed() {
+  document.body.style.backgroundColor = "red";
+}
+
+function changeColorYellow() {
+  document.body.style.backgroundColor = "yellow";
+}
+
+blue.addEventListener("click", changeColorBlue);
+red.addEventListener("click", changeColorRed);
+yellow.addEventListener("click", changeColorYellow);
---
+++ calculator/script.js
@@ -0,0 +1,25 @@
+/* If button !== euqual {
+  while true {
+    Click Button
+    Value of button appears in Screen
+    Value saved
+  }
+
+  else == clear {
+    delete last value
+  }
+
+  else {
+    solve equation
+    print to screen
+    stop
+  }
+  */
+
+
+  /*  Authors Styles */
+
+
+
+
+  
