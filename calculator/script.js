86cecf2e45472fd7dc651c19d7645e0c8dddc215
hey-jessica-j <hellojessicajennings@gmail.com>
2021-02-08 17:04:28 -0500

    calc adding js

--- calculator/script.js
+++ calculator/script.js
@@ -14,12 +14,10 @@
     print to screen
     stop
   }
+
+  1. set up so that when you chose a button it changes the screen to the Number
+  2. set it up so it keeps the values on the Screen
+  3. If you push clear the equation is wiped clean
+  4. When you push euqal the equation is solved
+  5. when you push the next button the value is wipped away
   */
-
-
-  /*  Authors Styles */
-
-
-
-
-  
